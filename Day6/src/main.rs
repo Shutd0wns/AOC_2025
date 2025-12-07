f304bb6643fdc5a87e8adcf3b79c28d39ece974b
Shutd0wns <radek.piterak@gmail.com>
2025-12-07 01:18:46 +0100

    Day 6 - finished

--- Day6/src/main.rs
+++ Day6/src/main.rs
@@ -19,6 +19,26 @@ fn solve_puzzle(file_path: &str) -> Result<(u64, u64), String> {
         Err(e) => return Err(e.to_string())
     };
 
+    let solution1 = match solve_part1(file) {
+        Ok(val) => val,
+        Err(e) => return Err(e)
+    };
+
+    let file_result = File::open(file_path);
+    let file = match file_result {
+        Ok(value) => value,
+        Err(e) => return Err(e.to_string())
+    };
+
+    let solution2 = match solve_part2(file) {
+        Ok(val) => val,
+        Err(e) => return Err(e)
+    };
+
+    Ok((solution1,solution2))
+}
+
+fn solve_part1(file: File) -> Result<u64, String> {
     let reader = BufReader::new(file);
 
     let num_reg = Regex::new(r"\d+").unwrap();
@@ -42,7 +62,7 @@ fn solve_puzzle(file_path: &str) -> Result<(u64, u64), String> {
                     _ => numbers.iter().map(|x| x[index]).fold(1, |acc, x| acc * x)
                 }
             }
-            break
+            return Ok(sum_of_solutions);
         }
 
         let mut row: Vec<u64> = Vec::new();
@@ -57,6 +77,52 @@ fn solve_puzzle(file_path: &str) -> Result<(u64, u64), String> {
         numbers.push(row);
     }
 
+    Err("No operators found".to_string())
+}
 
-    Ok((sum_of_solutions,0))
+fn solve_part2(file: File) -> Result<u64, String> {
+    let mut reader = BufReader::new(file);
+
+    let op_reg = Regex::new(r"[\+\*]").unwrap();
+
+    let mut buffer: String = String::new();
+    match reader.read_line(&mut buffer) {
+        Ok(_) => {},
+        Err(e) => return Err(e.to_string())
+    }
+    let mut strings: Vec<String> = buffer.strip_suffix("\r\n").unwrap().chars().map(|x| if x == ' ' {"".to_string()} else {x.to_string()}).collect();
+
+    for line_result in reader.lines() {
+        let line = match line_result {
+            Ok(val) => val,
+            Err(e) => return Err(e.to_string())
+        };
+
+        let mut sum_of_solutions: u64 = 0;
+
+        if op_reg.is_match(line.as_str()) {
+            let ops: Vec<&str> = op_reg.find_iter(line.as_str()).map(|x| x.as_str()).collect();
+            let num_groups: Vec<Vec<_>> = strings.split(|x| x == "")
+                .map(|x| x.iter().map(|y| y.parse::<u64>().unwrap()).collect()).collect();
+
+            for index in 0..ops.len() {
+                sum_of_solutions += match ops[index] {
+                    "+" => num_groups[index].iter().sum::<u64>(),
+                    _ => num_groups[index].iter().fold(1, |acc, x| acc * x)
+                }
+            }
+            return Ok(sum_of_solutions);
+        }
+
+        let mut index = 0;
+        for char in line.chars() {
+            if char != ' ' {
+                strings[index].push(char);
+            }
+            index += 1;
+        }
+    }
+
+
+    Err("No operators found".to_string())
 }
